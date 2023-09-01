5c0c13e897116ce323545c6597e949d0addf91b0
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-01 18:20:08 -0400

    phase-1-functions-lab
    phase-1-functions-lab

--- phase-1-functions-lab-master/index.js
+++ phase-1-functions-lab-master/index.js
@@ -1,7 +1,7 @@
 describe('index.js', function() {
-    describe('distanceFromHqInBlocks()', function() {
-      it('returns a distance in blocks', function() {
-        expect(distanceFromHqInBlocks(43)).to.equal(1);
-      });
+  describe('distanceFromHqInBlocks()', function() {
+    it('returns a distance in blocks', function() {
+      expect(distanceFromHqInBlocks(43)).to.equal(1);
     });
   });
+});
