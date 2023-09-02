87b662b75e3dc7149423c715b356a92f11191514
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-01 22:19:10 -0400

    phase-1-functions-lab/
    phase-1-functions-lab/

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-function distanceFromHqInBlocks(block) {
-  // calculate the distance in blocks from headquarters
-  return Math.abs(block - 42); // assuming headquarters is at block 42
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot calculate fare";
+  }
 }
