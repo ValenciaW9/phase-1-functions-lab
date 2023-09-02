697c25f95c3468ad4e19452cad3a1984ef9f7b34
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-01 21:56:03 -0400

    phase-1-functions-lab/
    phase-1-functions-lab/

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+// test/indexTest.js
+
+const { expect } = require('chai'); // Assuming you're using Chai for assertions
+const { distanceFromHqInBlocks } = require('../index'); // Replace '../index' with the correct path to your 'index.js' file
+
+describe('index.js', function() {
+  describe('distanceFromHqInBlocks()', function() {
+    it('returns a distance in blocks', function() {
+      // Test case 1: Distance from headquarters is 43 blocks
+      const result1 = distanceFromHqInBlocks(43);
+      expect(result1).to.equal(1);
+
+      // You can add more test cases here for different input values
+    });
+  });
+});
--- phase-1-functions-lab-master/index.js
+++ phase-1-functions-lab-master/index.js
@@ -1,25 +1,16 @@
-const { distanceFromHqInBlocks, distanceFromHqInFeet, distanceTravelledInFeet, calculatesFarePrice } = require('./helpers.js');
+// test/indexTest.js
 
-function distanceFromHqInBlocks(blocks) {
-    // Implementation here
-  }
-  
+const { expect } = require('chai'); // Assuming you're using Chai for assertions
+const { distanceFromHqInBlocks } = require('../index'); // Replace '../index' with the correct path to your 'index.js' file
 
-  function distanceFromHqInFeet(feet) {
-    // Implementation here
-  }
-  
-  function distanceTravelledInFeet(start, destination) {
-    // Implementation here
-  }
-  
-  function calculatesFarePrice(start, destination) {
-    // Implementation here
-  }
-  
-  module.exports = {
-    distanceFromHqInBlocks,
-    distanceFromHqInFeet,
-    distanceTravelledInFeet,
-    calculatesFarePrice
-  };
+describe('index.js', function() {
+  describe('distanceFromHqInBlocks()', function() {
+    it('returns a distance in blocks', function() {
+      // Test case 1: Distance from headquarters is 43 blocks
+      const result1 = distanceFromHqInBlocks(43);
+      expect(result1).to.equal(1);
+
+      // You can add more test cases here for different input values
+    });
+  });
+});
