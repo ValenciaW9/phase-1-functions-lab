6f52374018398d8320caf6da4e16629f2d97c4c9
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-01 22:08:37 -0400

    phase-1-functions-lab/
    phase-1-functions-lab/

--- phase-1-functions-lab-master/index.js
+++ phase-1-functions-lab-master/index.js
@@ -1,16 +1,4 @@
-// test/indexTest.js
-
-const { expect } = require('chai'); // Assuming you're using Chai for assertions
-const { distanceFromHqInBlocks } = require('../index'); // Replace '../index' with the correct path to your 'index.js' file
-
-describe('index.js', function() {
-  describe('distanceFromHqInBlocks()', function() {
-    it('returns a distance in blocks', function() {
-      // Test case 1: Distance from headquarters is 43 blocks
-      const result1 = distanceFromHqInBlocks(43);
-      expect(result1).to.equal(1);
-
-      // You can add more test cases here for different input values
-    });
-  });
-});
+function distanceFromHqInBlocks(block) {
+  // calculate the distance in blocks from headquarters
+  return Math.abs(block - 42); // assuming headquarters is at block 42
+}
